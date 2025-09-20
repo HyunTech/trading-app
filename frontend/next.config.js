5724b7602259deb077c5447847ccaa801d900709
HyunTech <timue0829@gmail.com>
2025-09-21 00:33:37 +0900

    chore: ignore ESLint errors during production build

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ESLint 에러가 있어도 프로덕션 빌드를 진행
+    ignoreDuringBuilds: true,
+  },
+};
+module.exports = nextConfig;
